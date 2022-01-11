0b190cfaf69c86ed75cc00228918e6cb8fce66f0
kaulex <kaupp.alexis@hotmail.fr>
2022-01-11 16:37:16 +0100

    pushed repo

---
+++ API/TMDBapi.js
@@ -0,0 +1,20 @@
+
+const API_TOKEN = "1ac9a30945ec3410a7e887111d24f233";
+
+export function getFilmsFromApiWithSearchedText (text, page){
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + "&page=" + page
+
+    return fetch(url)
+    .then((response)=> response.json()
+    .catch((eroor)=> alert(error)))
+}
+
+export function getImageFromApi (name) {
+    return 'https://image.tmdb.org/t/p/w300' + name
+}
+
+export function getFilmDetailFromApi (id) {
+    return fetch('https://api.themoviedb.org/3/movie/' + id + '?api_key=' + API_TOKEN + '&language=fr')
+      .then((response) => response.json())
+      .catch((error) => console.error(error));
+  }
